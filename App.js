a6f8ed303f6d292205ae99ec8722f93a7fdfe18e
Evan <evanz1215@gmail.com>
2024-05-05 20:46:20 +0800

    Dynamically route header title

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
@@ -31,7 +31,16 @@ export default function App() {
               headerTitle: "Meal Categories",
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={({ route, navigation }) => {
+              const catId = route.params.categoryId;
+              return {
+                title: catId,
+              };
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
